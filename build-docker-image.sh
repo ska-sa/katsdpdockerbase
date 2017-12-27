39b082ec8bbf64e96db1e2716eaf3af3fc49b20b
Bruce Merry <bmerry@gmail.com>
2017-12-27 15:09:33 +0200

    Transfer build-docker-image.sh to katsdpdockerbase
    This was originally in the Jenkins slave image, but that means that it
    can't be updated without restarting the slaves, and packages can't
    select an alternative version of it.
    
    This version has also been adjusted to be compatible with a Python 3
    virtualenv.

---
+++ build-docker-image.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+if [ "$#" -lt 1 ]; then
+    echo "Usage: build-docker-image.sh image-name [args]" 1>&2
+    exit 1
+fi
+if [ -z "$DOCKER_REGISTRY" ]; then
+    echo "DOCKER_REGISTRY is not set" 1>&2
+    exit 1
+fi
+if [ -z "$BRANCH_NAME" ]; then
+    echo "BRANCH_NAME is not set" 1>&2
+    exit 1
+fi
+NAME="$1"
+shift
+LABEL="${BRANCH_NAME#origin/}"
+if [ "$LABEL" = "master" ]; then
+    LABEL=latest
+fi
+# katversion depends on interrogating git to find the version number, but if
+# the directory containing the Dockerfile isn't the top-level directory, or if
+# .git is listed in .Dockerignore, then it won't find it. It has a fallback of
+# consulting a file called ___version___, so we extract the version from
+# outside Docker and create that file. Note that this code needs to work on
+# Python 2 and 3.
+if [ -n "$VIRTUAL_ENV" ]; then
+    pip install katversion
+    python -c 'import katversion; print(katversion.get_version())' > ___version___
+fi
+if [ -x "scripts/docker_build.sh" ]; then
+    docker_build="scripts/docker_build.sh"
+else
+    docker_build="docker build"
+fi
+$docker_build --label=org.label-schema.schema-version=1.0 \
+              --label=org.label-schema.vcs-ref="$(git rev-parse HEAD)" \
+              --label=org.label-schema.vcs-url="$(git remote get-url origin)" \
+              --pull=true --no-cache=true --force-rm=true \
+              -t "$DOCKER_REGISTRY/$NAME:$LABEL" "$@" .
+# Remove the image, whether push is successful or not, to avoid accumulating
+# more and more images on the build slaves. This is skipped for Jenkins
+# images, since they are actually used on the build machines.
+if [[ "$NAME" != jenkins-* ]]; then
+    trap "docker rmi $DOCKER_REGISTRY/$NAME:$LABEL" EXIT
+fi
+rm -f ___version___
+docker push "$DOCKER_REGISTRY/$NAME:$LABEL"
